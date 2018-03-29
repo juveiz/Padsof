02e88fef6a041e23f5acc56681cb9166e7501380
SavageLaoch <juveiz2931@gmail.com>
2018-03-29 14:27:27 +0200

    Javadoc

--- Practica 3/User/Guest.java
+++ Practica 3/User/Guest.java
@@ -5,25 +5,44 @@ import java.util.List;
 
 import offer.Offer;
 
+/**
+ * Guest class. It represents the registered user Guests.
+ * 
+ * @author Miguel Angel Sanchez y Juan Velasco
+ *
+ */
 public class Guest extends Profile {
-	
+	/**
+	 * Offers: List of reserved offers
+	 */
 	private List<Offer> offers;
-	
+	/**
+	 * Creates a new Guest profile
+	 */
 	public Guest() {
 		offers = new ArrayList<>();
 	}
-	
+	/**
+	 * Add and offer to the Guest
+	 * @param o Offer to add
+	 */
 	public void addOffer(Offer o) {
 		offers.add(o);
 	}
-	
+	/**
+	 * Removes an offer from the Guest
+	 * @return true o false
+	 */
 	public boolean removeOffer(Offer o) {
 		if (offers.remove(o)) {
 			return true;
 		}
 		return false;
 	}
-	
+	/**
+	 * Says if the profile is a Guest
+	 * @return True
+	 */
 	public boolean isGuest() {
 		return true;
 	}
--- Practica 3/User/Profile.java
+++ Practica 3/User/Profile.java
@@ -2,25 +2,49 @@ package User;
 
 import offer.House;
 import offer.Offer;
-
+/**
+ * Profile class. It represents the type of registered user
+ * @author Miguel Angel Sanchez y Juan Velasco
+ *
+ */
 public abstract class Profile {
-	
+	/**
+	 * Add an offer to the profile. It will be used in its sons.
+	 * @param o House to add
+	 */
 	public void addOffer(Offer o) {};
-	
+	/**
+	 * Add a house to the profile. It will be used in its sons.
+	 * @param h
+	 */
 	public void addHouse(House h) {};
-	
+	/**
+	 * Removes an offer from the profile.
+	 * @param o Offer to be removed
+	 * @return true if you can remove the offer or false if the offer is not in the profile
+	 */
 	public boolean removeOffer(Offer o) {
 		return false;
 	}
-	
+	/**
+	 * Removes a house from the profile.
+	 * @param h House to be removed
+	 * @return true if you can remove the house or false if the house is not in the profile
+	 */
 	public boolean removeHouse(House h) {
 		return false;
 	}
-	
+	/**
+	 * Says if the profile is Guest
+	 * @return false
+	 */
 	public boolean isGuest() {
 		return false;
 	}
-	
+	/**
+	 * Says if the profile is Host
+	 * @return false
+	 */
 	public boolean isHost() {
 		return false;
 	}
