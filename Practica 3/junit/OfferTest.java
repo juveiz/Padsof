cee90f7a579ca182e6ff1ec6702bbb2b5abccab2
mikism98 <miguelangelsm98@hotmail.com>
2018-04-03 19:50:52 +0200

    offer test

---
+++ Practica 3/junit/OfferTest.java
@@ -0,0 +1,157 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import user.RegisteredUser;
+
+public class OfferTest {
+
+	@Test
+	public void testOffer() {
+		
+		assertNotNull(new Offer("Paco", "Sanchez","1","Wololo","1234a1234",true,false));
+	}
+
+	@Test
+	public void testGetStartingDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStartingDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetprice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetprice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetModifyDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetModifyDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHouse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetComments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReserve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReserve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApproveOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDenyOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAskForChanges() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCancelOfer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifyOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReserveOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuyOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCommentOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRateOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsLiving() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsVacational() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDeposit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDeposit() {
+		fail("Not yet implemented");
+	}
+
+}
