40b1061f827f421362bb1ba6140d5c456c543d6b
mikism98 <miguelangelsm98@hotmail.com>
2018-04-03 19:36:55 +0200

    javadoc offer

--- Practica 3/offer/Offer.java
+++ Practica 3/offer/Offer.java
@@ -60,46 +60,106 @@ public abstract class Offer implements Serializable{
 	public void setStartingDate(LocalDate startingDate) {
 		this.startingDate = startingDate;
 	}
+	
+	/**
+	 * Gets the price of the offer
+	 * @return price
+	 */
 	public double getprice() {
 		return price;
 	}
+	
+	/**
+	 * Sets the price of the offer
+	 * @param price Price of the offer
+	 */
 	public void setprice(double price) {
 		this.price = price;
 	}
+	
+	/**
+	 * Gets the state of the offer
+	 * @return state
+	 */
 	public int getState() {
 		return state;
 	}
+	
+	/**
+	 * Sets the state of the offer
+	 * @param state State of the offer
+	 */
 	public void setState(int state) {
 		this.state = state;
 	}
+	
+	/**
+	 * Gets the last date that the offer was modified
+	 * @return modify date
+	 */
 	public LocalDate getModifyDate() {
 		return modifyDate;
 	}
+	
+	/**
+	 * Sets the modify date
+	 * @param modifyDate Last date that the offer was modified
+	 */
 	public void setModifyDate(LocalDate modifyDate) {
 		this.modifyDate = modifyDate;
 	}
+	
+	/**
+	 * Gets the host that made the offer
+	 * @return host
+	 */
 	public RegisteredUser getHost() {
 		return host;
 	}
+	
+	/**
+	 * Gets the house of the offer
+	 * @return house
+	 */
 	public House getHouse() {
 		return house;
 	}
+	
+	/**
+	 * Gets the list of the comments of the offer
+	 * @return list of the comments
+	 */
 	public List<Comment> getComments() {
 		return comments;
 	}
+	
+	/**
+	 * Sets a list of comments to the offer
+	 * @param comments List of comments
+	 */
 	public void setComments(List<Comment> comments) {
 		this.comments = comments;
 	}
+	
+	/**
+	 * Gets the reserve of the offer
+	 * @return The reserve if it's reserved or null if it's not
+	 */
 	public Reserve getReserve() {
 		return reserve;
 	}
+	
+	/**
+	 * Sets a reserve to the offer
+	 * @param reserve Reserve for the offer
+	 */
 	public void setReserve(Reserve reserve) {
 		this.reserve = reserve;
 	}
 	
 	/**
-	 * a implementar (easy)
-	 * @return
+	 * Gets the rate of the offer
+	 * @return Rate of the offer
 	 */
 	public double getRate() {
 		double res = 0;
@@ -110,30 +170,52 @@ public abstract class Offer implements Serializable{
 				cont = cont + 1;
 			}
 		}
+		if (cont==0) return 0;
 		res = res/cont;
 		return res;
 	}
 	
+	/**
+	 * Approves the offer
+	 * @param a Admin that approves the offer
+	 */
 	public void approveOffer(Admin a) {
 		this.setState(1);
 		modifyDate = LocalDate.now();
 	}
 	
-	public boolean denyOffer(Admin a) throws HostException {
+	/**
+	 * Denies the offer
+	 * @param a Admin that denies the offer
+	 */
+	public void denyOffer(Admin a){
 		this.setState(-1);
-		return true;
 	}
 	
+	/**
+	 * Ask for changes in the offer
+	 */
 	public void askForChanges() {
 		this.setState(2);
 		modifyDate = LocalDate.now();
 	}
 	
+	/**
+	 * Cancels the offer
+	 * @throws HostException The user is not a host
+	 */
 	public void cancelOfer() throws HostException {
 		this.setState(-1);
 		host.getHost().removeOffer(this);
 	}
 	
+	/**
+	 * Modifies the offer
+	 * @param h New house of the offer
+	 * @param s New date of the offer
+	 * @param d New price of the offer
+	 * @return True if the offer was successfully modified, false if it was not
+	 */
 	public boolean modifyOffer(House h, LocalDate s, double d) {
 		if(modifyDate.plusDays(5).compareTo(LocalDate.now()) <= 0) {
 			this.setState(-1);
@@ -147,6 +229,12 @@ public abstract class Offer implements Serializable{
 		return true;
 	}
 	
+	/**
+	 * Reserves the offer
+	 * @param g Guest that reserves the offer
+	 * @return True if the offer is successfully reserved, false if it's not
+	 * @throws GuestException The user is not a guest
+	 */
 	public boolean reserveOffer(RegisteredUser g) throws GuestException {
 		if (g.isGuest() == false) {
 			GuestException gu = new GuestException();
@@ -160,13 +248,17 @@ public abstract class Offer implements Serializable{
 		return true;
 	}
 	
+	/*
+	 * HAY QUE HACER LO DE PAGAR AL HOST
+	 */
+	
 	/**
-	 * hay que hacer lo de pagar al host
-	 * @param g
-	 * @param subject
-	 * @param trace
-	 * @return
-	 * @throws GuestException
+	 * Buys this offer
+	 * @param g Guest that buys the offer
+	 * @param subject Subject of the buying of the offer
+	 * @param trace Trace of the payment of the offer
+	 * @return different numbers for each error
+	 * @throws GuestException The user is not a guest
 	 */
 	public int buyOffer(RegisteredUser g,String subject,boolean trace) throws GuestException {
 		String creditCard;
@@ -227,6 +319,12 @@ public abstract class Offer implements Serializable{
 		
 	}
 	
+	/**
+	 * Comments the offer
+	 * @param g Guest that comments the offer
+	 * @param c String with the comment
+	 * @throws GuestException The user is not a guest
+	 */
 	public void commentOffer(RegisteredUser g,String c) throws GuestException {
 		if (g.isGuest() == false) {
 			GuestException gu = new GuestException();
@@ -237,6 +335,12 @@ public abstract class Offer implements Serializable{
 		comments.add(comment);
 	}
 	
+	/**
+	 * Rates the offer
+	 * @param g Guest that rates the offer
+	 * @param r Rating of the offer
+	 * @throws GuestException the user is not a guest
+	 */
 	public void rateOffer(RegisteredUser g, double r) throws GuestException{
 		if (g.isGuest() == false) {
 			GuestException gu = new GuestException();
@@ -247,18 +351,34 @@ public abstract class Offer implements Serializable{
 		comments.add(comment);
 	}
 	
+	/**
+	 * Checks if the offer is a living offer
+	 * @return True if it's a living offer, false if it's not
+	 */
 	public boolean isLiving() {
 		return false;
 	}
 	
+	/**
+	 * Checks if the offer is a vacational offer
+	 * @return True if it's a vacational offer, false if it's not
+	 */
 	public boolean isVacational() {
 		return false;
 	}
 
+	/**
+	 * Gets the deposit of the offer
+	 * @return deposit
+	 */
 	public double getDeposit() {
 		return deposit;
 	}
-
+	
+	/**
+	 * Sets the deposit of the offer
+	 * @param deposit Deposit of the offer
+	 */
 	public void setDeposit(double deposit) {
 		this.deposit = deposit;
 	}
