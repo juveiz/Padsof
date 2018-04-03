d32f1189aba67e6a27b43006a4ed3cf42a4b7eab
SavageLaoch <juveiz2931@gmail.com>
2018-04-03 19:53:03 +0200

    leer de fichero terminado

--- Practica 3/system/MainTest.java
+++ Practica 3/system/MainTest.java
@@ -2,21 +2,22 @@ package system;
 
 import java.io.*;
 
-import system.*;
+/*import system.*;
 import comments.*;
 import exception.*;
 import offer.*;
-import user.*;
+import user.*;*/
 
 public class MainTest {
 		public static void main(String[] args) {
+			Application app = new Application("Eva","Touris","BolasAbiertas");
+			
 			try{
 				FileInputStream f = null;
 				String line;
-				f = new FileInputStream("ClientsExamples.txt");
+				f = new FileInputStream("ClientsExample.txt");
 				BufferedReader buffer = new BufferedReader(new InputStreamReader(f));
-				Application app = new Application("Eva","Touris","BolasAbiertas");
-				line = buffer.readLine();
+				buffer.readLine();
 				while((line = buffer.readLine()) != null) {
 					boolean guest,host;
 					String[] newUser = line.split(";");
@@ -36,9 +37,11 @@ public class MainTest {
 					app.addUser(newUser[3],newUser[2],newUser[1],newUser[4],newUser[5],guest,host);
 				}
 				buffer.close();
+				System.out.println("Hecho");
 			}catch(IOException e) {
-		
+				System.out.println("The given file hasn't been opened");
+				return;
 			}
-		
+			
 		}
 }
