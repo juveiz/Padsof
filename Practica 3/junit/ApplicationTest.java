33101b2202cf6c4ba2e8e95af3b64579b4781198
SavageLaoch <juan.velascoi@estudiante.uam.es>
2018-04-06 09:49:55 +0200

    y ahora colocao

--- Practica 3/junit/ApplicationTest.java
+++ Practica 3/junit/ApplicationTest.java
@@ -38,7 +38,7 @@ public class ApplicationTest {
 
 	@Test
 	public void testLoginAdmin() {
-		Application app = Application.getApplication("Eva", "Touris","OpenBalls");
+		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		Admin admin = null;
 		try {
 			admin = app.loginAdmin("ImTheMdfkAdmin", "OpenBalls");
@@ -70,7 +70,7 @@ public class ApplicationTest {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addHouse("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
-			assertEquals(1,app.getHostHouses(user).size());
+			assertEquals(1, app.getHostHouses(user).size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -85,7 +85,7 @@ public class ApplicationTest {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addHouse("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
-			assertEquals(1,app.getHostHouses(user).size());
+			assertEquals(1, app.getHostHouses(user).size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -95,14 +95,14 @@ public class ApplicationTest {
 	public void testAddOfferLiving() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.searchOfferType("Living").size());
+			assertEquals(1, app.searchOfferType("Living").size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -112,14 +112,14 @@ public class ApplicationTest {
 	public void testAddOfferVacational() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferVacational(LocalDate.now(), 100, house, LocalDate.now().plusDays(3));
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.searchOfferType("Vacational").size());
+			assertEquals(1, app.searchOfferType("Vacational").size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -129,14 +129,14 @@ public class ApplicationTest {
 	public void testGetHostOffers() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferVacational(LocalDate.now(), 100, house, LocalDate.now().plusDays(3));
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.getHostOffers(user).size());
+			assertEquals(1, app.getHostOffers(user).size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -146,14 +146,14 @@ public class ApplicationTest {
 	public void testGetGuestOffers() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferVacational(LocalDate.now(), 100, house, LocalDate.now().plusDays(3));
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(0,app.getGuestOffers(user).size());
+			assertEquals(0, app.getGuestOffers(user).size());
 		} catch (LoggedException | BannedUserException | HostException | GuestException e) {
 			fail("Can't loggin");
 		}
@@ -163,7 +163,7 @@ public class ApplicationTest {
 	public void testGetAdminOffers() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
@@ -171,7 +171,7 @@ public class ApplicationTest {
 			app.addOfferVacational(LocalDate.now(), 100, house, LocalDate.now().plusDays(3));
 			user.setState(0);
 			app.loginAdmin("ImTheMdfkAdmin", "OpenBalls");
-			assertEquals(1,app.getAdminOffers().size());
+			assertEquals(1, app.getAdminOffers().size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -181,35 +181,35 @@ public class ApplicationTest {
 	public void testSearchOfferType() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.searchOfferType("Living").size());
+			assertEquals(1, app.searchOfferType("Living").size());
 			app.addOfferVacational(LocalDate.now(), 100, house, LocalDate.now().plusDays(3));
 			app.getHostOffers(user).get(1).setState(1);
-			assertEquals(1,app.searchOfferType("Vacational").size());
+			assertEquals(1, app.searchOfferType("Vacational").size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
-		
+
 	}
 
 	@Test
 	public void testSearchOfferZip() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.searchOfferZip(28903).size());
+			assertEquals(1, app.searchOfferZip(28903).size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -219,14 +219,14 @@ public class ApplicationTest {
 	public void testSearchOfferDate() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(1,app.searchOfferDate(LocalDate.now().minusDays(10),LocalDate.now().plusDays(10)).size());
+			assertEquals(1, app.searchOfferDate(LocalDate.now().minusDays(10), LocalDate.now().plusDays(10)).size());
 		} catch (LoggedException | BannedUserException | HostException e) {
 			fail("Can't loggin");
 		}
@@ -236,14 +236,14 @@ public class ApplicationTest {
 	public void testSearchOfferRate() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(1);
-			assertEquals(0,app.searchOfferRate(3).size());
+			assertEquals(0, app.searchOfferRate(3).size());
 		} catch (LoggedException | BannedUserException | HostException | NonRegisteredException e) {
 			fail("Can't loggin");
 		}
@@ -253,14 +253,14 @@ public class ApplicationTest {
 	public void testSearchOfferReserved() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(3);
-			assertEquals(1,app.searchOfferReserved().size());
+			assertEquals(1, app.searchOfferReserved().size());
 		} catch (LoggedException | BannedUserException | HostException | NonRegisteredException e) {
 			fail("Can't loggin");
 		}
@@ -270,14 +270,14 @@ public class ApplicationTest {
 	public void testSearchOfferBought() {
 		Application app = Application.getApplication("Eva", "Touris", "OpenBalls");
 		RegisteredUser user = null;
-		House house = new House ("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
+		House house = new House("Madrid", 28903, "Avenida de la Felicidad nº 23.\n Very wide\nPool");
 		app.addUser("Paco", "Sanchez", "1", "Wololo", "1234a1234", true, true);
 		try {
 			user = app.loginUser("1", "Wololo");
 			assertNotNull(user);
 			app.addOfferLiving(LocalDate.now(), 200, house, 3);
 			app.getHostOffers(user).get(0).setState(4);
-			assertEquals(1,app.searchOfferBought().size());
+			assertEquals(1, app.searchOfferBought().size());
 		} catch (LoggedException | BannedUserException | HostException | NonRegisteredException e) {
 			fail("Can't loggin");
 		}
